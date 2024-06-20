465505023e9929fde31bc05daf02d10ddf3af7e7
YelenaZhou <yelenazhou2021@gmail.com>
2024-06-20 09:48:30 -0400

    .

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// let open = document.querySelector(".name");
+// let open2 = document.querySelector(".name2");
+
+// open.addEventListener("mouseover", function() {
+//     open2.style.display = "block";
+//     open2.style.display = "flex";
+// })
+
+// open.addEventListener("mouseout", function() {
+//     open2.style.display = "none";
+// })
